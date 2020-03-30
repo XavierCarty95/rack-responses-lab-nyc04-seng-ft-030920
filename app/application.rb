0d570eb84dc3cde9fcc4a39eec4472b6be51c42c
Xavier Carty <xaviercarty@gmail.com>
2020-03-30 01:24:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     res = Rack::Response.new 
     time = Time.new
     res.write "#{time.hour}\n"
-    if time.hour < 12 
+    if time.hour <= 12 
       res.write "Good Morning"
     else 
       res.write "Good Afternoon"
